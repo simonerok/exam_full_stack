7c922b370d33dce9858d69cb439ffd8a64dddbba
Sofia S. Rokkedal <ssimone12@gmail.com>
2024-11-13 10:52:25 +0100

    styling changes

--- frontend/src/app/components/Projects.tsx
+++ frontend/src/app/components/Projects.tsx
@@ -15,24 +15,22 @@ const Projects: React.FC = () => {
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-8">
           {projects.length > 0 ? (
             projects.map((project) => (
-              <div key={project.id} className="bg-white rounded-lg shadow-lg hover:shadow-xl overflow-hidden transition-transform transform hover:-translate-y-1 flex flex-col" style={{ height: "400px" }}>
+              <div key={project.id} className="relative bg-white rounded-lg shadow-lg hover:shadow-xl overflow-hidden transition-transform transform hover:-translate-y-1 flex flex-col" style={{ height: "400px" }}>
                 {/* Image */}
-                <div className="flex-grow">
-                  <img src={project.img_url || "/splash.avif"} alt={project.name} className="w-full h-full object-cover" />
-                </div>
+                <img src={project.img_url || "/splash.avif"} alt={project.name} className="w-full h-full object-cover absolute inset-0 z-0" />
 
                 {/* Overlay for text */}
-                <div className="bg-white text-bg_darker bg-opacity-60 p-4 flex flex-col justify-end">
-                  {/* Tags */}
+                <div className="absolute inset-0 bg-black bg-opacity-50 z-10 p-4 flex flex-col justify-end">
+                  {/* Tags - hardcoded for now*/}
                   <div className="flex flex-wrap gap-2 mb-2">
                     {["Design", "Development", "WordPress"].map((tag, index) => (
-                      <span key={index} className="text-xs text-bg_darker border border-bg_darker rounded-[9999px] px-2 py-1">
+                      <span key={index} className="text-xs text-white border border-white rounded-[9999px] px-2 py-1">
                         {tag}
                       </span>
                     ))}
                   </div>
                   {/* Title */}
-                  <h2 className="text-lg font-semibold text-bg_darker text-left">{project.name}</h2>
+                  <h2 className="text-lg font-semibold text-white text-left">{project.name}</h2>
                 </div>
               </div>
             ))
